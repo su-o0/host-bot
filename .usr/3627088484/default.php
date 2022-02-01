733d285c59f2a45a2946d3290b3b222d92501017
su-o0 <plitikyura@gmail.com>
2022-02-01 17:14:21 +0200

    default bot

--- .usr/3627088484/default.php
+++ .usr/3627088484/default.php
@@ -1,1 +1,40 @@
 <?php
+if(!defined("ACCESS")) { error_code_503();}
+
+include "sqlite.php";
+
+if ($method == "message") {
+    if ($type != "text") {
+
+        // apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => "Ремонтые работы..."));
+        exit;
+    }
+    else if ($is_cmd) {
+        $command;
+        exit;
+    } 
+    else if ($is_custom_cmd) {
+        switch ($command) {
+            default:
+                exit;
+            break;
+        }
+    }
+} 
+else if ($method == "callback") {
+    if(!$is_callback_data_explode){
+        switch ($callback_data) {
+            default:
+                exit;
+            break;
+        }
+    }
+    else {
+        switch ($explode_callback_data[0]) {
+            default:
+                exit;
+            break;
+        }
+    }
+
+}
