96df32b5397a91353fe4a34392ee84328bd78bb6
su-o0 <plitikyura@gmail.com>
2022-02-01 14:06:05 +0200

    add router file index.php

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
 <?php
 
+function error_code_503(){
+    header('HTTP/1.1 503 Service Temporarily Unavailable');
+    exit;
+}
+
+if (!isset($_GET["AA"]))
+    error_code_503();
+
+$token = ("./.token/".sha1($_GET['AA']).".json");
+
+if(!file_exists($token))
+    error_code_503();
+
+$perms = fileperms($token);
+
+if((fileperms("./.token/") & 0x0002) or (fileperms($token) & 0x0002)){
+    echo "check token permision";
+    exit;
+}
+$token_data = file_get_contents($token);
+
+if($token_data === false)
+    error_code_503();
+
+$json = json_decode($token_data, true);
+
+define('bot_id', $_GET["AA"]);
+define('BOT_TOKEN', $json[0]);
+
+include "core.php";
